6185dd5abb69219f6329a84bc6fa1bc9ad122cc8
kasparmartens <kaspar.martens@gmail.com>
2020-03-04 14:56:33 +0000

    try setting up a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BasisVAE',
+      version='0.1',
+      description='Implementation of BasisVAE',
+      author='Kaspar Märtens',
+      author_email='kaspar.martens@spc.ox.ac.uk',
+      url='https://github.com/kasparmartens/BasisVAE',
+      license='MIT',
+      packages=['BasisVAE'])
